8e7b0a2940cef69b7b733105edad6cb7b59e803f
scolear <bobtera@gmail.com>
2019-12-03 13:45:45 +0100

    croos platform image path test

--- main.cpp
+++ main.cpp
@@ -1,12 +1,21 @@
+#ifdef _WIN32
+#define PATH "..\\..\\..\\"
+#endif
+
+#ifdef linux
+#define PATH "../"
+#endif
+
 #include <stdio.h>
 #include <opencv2/opencv.hpp>
+
 
 using namespace cv;
 int main(int argc, char** argv )
 {
 
     Mat image;
-    image = imread("..\\..\\..\\cyberpunk_room.jpg", 1);
+    image = imread(PATH "cyberpunk_room.jpg", 1);
     if ( !image.data )
     {
         printf("No image data \n");
