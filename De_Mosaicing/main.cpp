a38b395e9a9d7f92bd60a855be58f189fe2ee07e
Dodoo86 <l.davidtamas@gmail.comm>
2019-12-06 09:44:21 +0100

    Demosaicing green done

--- De_Mosaicing/main.cpp
+++ De_Mosaicing/main.cpp
@@ -24,56 +24,16 @@
 
 cv::Mat image;
 std::vector<cv::Mat> channels;
-int rows;
-int colums;
-/*
-cv::Mat demosaicing_nearest_neighbor(cv::Mat image, std::vector<cv::Mat> channels, int rows, int colums)
+//int rows;
+//int colums;
+
+
+
+cv::Mat demosaicing_nearest_neighbor(cv::Mat image)
 {
     cv::split(image, channels);
-
-    cv::Mat ch1, ch2, ch3, dst;
-    ch1 = channels[0];
-    ch2 = channels[1];
-    ch3 = channels[2];
-
-    for (int i = 1; i < rows - 2; ++i) {
-        for (int j = 1; j < colums - 2; ++j)
-        {
-            if(ch2.at<uchar>(i, j)==0)
-            {
-                ch2.at<uchar>(i, j) = ch2.at<uchar>(i, j - 1);
-            }
-            if(ch1.at<uchar>(i, j) == 0)
-            {
-                ch1.at<uchar>(i, j) = ch1.at<uchar>(i, j -1);
-            }
-            if(ch1.at<uchar>(i, j) == 0)
-            {
-                ch1.at<uchar>(i, j) = (ch1.at<uchar>(i + 1, j) + ch1.at<uchar>(i - 1, j)) /2;
-            }
-            if(ch3.at<uchar>(i, j) == 0)
-            {
-                ch3.at<uchar>(i, j) = ch3.at<uchar>(i, j - 1);
-            }
-            if(ch3.at<uchar>(i, j) == 0)
-            {
-                ch3.at<uchar>(i, j) = (ch3.at<uchar>(i + 1, j) + ch3.at<uchar>(i - 1, j))/2;
-            }
-        }
-    }
-    cv::merge(channels, dst);
-    return dst;
-}
- */
-int main() {
-    cv::Mat image;
-    image = cv::imread("../parrot-raw.png",1);
-    if(!image.data)
-    {
-        printf("No image data !!!");
-        return -1;
-    }
-    cv::split(image, channels);
+    int rows = image.rows;
+    int colums = image.cols;
 
     cv::Mat ch1, ch2, ch3, dst;
     ch1 = channels[0];
@@ -87,19 +47,39 @@ int main() {
             {
                 ch2.at<uchar>(i, j) = (ch2.at<uchar>(i, j - 1) + ch2.at<uchar>(i, j + 1) + ch2.at<uchar>(i, j - 1) + ch2.at<uchar>(i + 1, j))/4;
             }
-            else if(ch1.at<uchar>(i, j) == 0)
+        }
+    }
+    for (int i = 1; i < rows - 2; ++i) {
+        for (int j = 1; j < colums - 2; ++j)
+        {
+             if(ch1.at<uchar>(i, j) == 0)
             {
                 ch1.at<uchar>(i, j) = (ch1.at<uchar>(i, j -1) + ch1.at<uchar>(i, j+1) + ch1.at<uchar>(i, j-1) + ch1.at<uchar>(i + 1, j))/4;
             }
-            else if(ch1.at<uchar>(i, j) == 0 && ch1.at<uchar>(i + 1, j - 1) != 0 && ch1.at<uchar>(i - 1, j) != 0)
+        }
+    }
+    for (int i = 1; i < rows - 2; ++i) {
+        for (int j = 1; j < colums - 2; ++j)
+        {
+            if(ch1.at<uchar>(i, j) == 0 && ch1.at<uchar>(i + 1, j - 1) != 0 && ch1.at<uchar>(i - 1, j) != 0)
             {
                 ch1.at<uchar>(i, j) = (ch1.at<uchar>(i + 1, j) + ch1.at<uchar>(i - 1, j)) /2;
             }
-            else if(ch3.at<uchar>(i, j) == 0)
+        }
+    }
+    for (int i = 1; i < rows - 2; ++i) {
+        for (int j = 1; j < colums - 2; ++j)
+        {
+            if(ch3.at<uchar>(i, j) == 0)
             {
                 ch3.at<uchar>(i, j) = (ch3.at<uchar>(i, j - 1) + ch3.at<uchar>(i, j + 1) + ch3.at<uchar>(i, j - 1) + ch3.at<uchar>(i + 1, j))/4;
             }
-            else if(ch3.at<uchar>(i, j) == 0 && ch3.at<uchar>(i +1, j -1) != 0 && ch3.at<uchar>(i - 1, j) != 0)
+        }
+    }
+    for (int i = 1; i < rows - 2; ++i) {
+        for (int j = 1; j < colums - 2; ++j)
+        {
+            if(ch3.at<uchar>(i, j) == 0 && ch3.at<uchar>(i +1, j -1) != 0 && ch3.at<uchar>(i - 1, j) != 0)
             {
                 ch3.at<uchar>(i, j) = (ch3.at<uchar>(i + 1, j) + ch3.at<uchar>(i - 1, j))/2;
             }
@@ -107,8 +87,22 @@ int main() {
     }
     cv::merge(channels, dst);
     cv::blur(dst, dst, cv::Size(4,4), cv::Point(-1, -1),1);
+    return dst;
+}
+
+int main() {
+    cv::Mat image;
+
+    image = cv::imread("../parrot-raw.png",1);
+    if(!image.data)
+    {
+        printf("No image data !!!");
+        return -1;
+    }
+    cv::Mat dst = demosaicing_nearest_neighbor(image);
     cv::namedWindow("Image Resoult", cv::WINDOW_AUTOSIZE);
     cv::namedWindow("Image Original", cv::WINDOW_AUTOSIZE);
+
     cv::imshow("Image Resoult", dst);
     cv::imshow("Image Original", image);
     cv::waitKey(0);
